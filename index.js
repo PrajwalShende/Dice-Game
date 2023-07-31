9c49e6ceee9bd312fc4157f83d1544e48647983c
Prajwal Shende <131299565+PrajwalShende@users.noreply.github.com>
2023-07-31 23:39:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//player1
+
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomDiceImage="dice" + randomNumber1 +".png";
+
+var randomImageSrc ="images/" + randomDiceImage;
+
+
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute('src',randomImageSrc);
+
+//player2
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImageSrc="images/dice" + randomNumber2 + ".png";
+
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute('src',randomImageSrc);
+
+
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 1 wins!"; 
+}
+else if
+    (randomNumber1<randomNumber2) {
+        document.querySelector("h1").innerHTML="Player 2 wins!"; 
+    } else { document.querySelector("h1").innerHTML="Draw!"; }
